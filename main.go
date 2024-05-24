b41defc7662240ab288d7f09a235e0b4482f4f89
isacben <ibsandoval@gmail.com>
2024-05-24 07:45:46 -0700

    add read yaml file

--- main.go
+++ main.go
@@ -3,13 +3,44 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"gopkg.in/yaml.v3"
 )
+
+type Prog struct {
+	Steps []Step `yaml:"steps"`
+}
+
+type Step struct {
+	Name   string `yaml:"name"`
+	Method string `yaml:"method"`
+	URL    string `yaml:"url"`
+}
 
 func main() {
 	var clientId = os.Getenv("clientId")
 	var apiKey = os.Getenv("apiKey")
 
 	// load yaml file
+	prog := Prog{}
+
+	yamlFile, err := os.ReadFile("../poget-examples/get-customer.yaml")
+	if err != nil {
+		fmt.Printf("yamlFile.Get err #%v ", err)
+	}
+	err = yaml.Unmarshal(yamlFile, &prog)
+	if err != nil {
+		fmt.Printf("Unmarshal: %v", err)
+	}
+
+	fmt.Printf("--- prog:\n%v\n\n", prog)
+
+	// dump
+	d, err := yaml.Marshal(&prog)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+	fmt.Printf("--- prog dump:\n%s\n\n", string(d))
 
 	fmt.Println(clientId, apiKey)
 }
